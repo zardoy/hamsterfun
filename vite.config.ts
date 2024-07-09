7635c0ee733057aef76298ff85fe60003f885a34
Vitaly Turovsky <vital2580@icloud.com>
2024-07-09 15:31:59 +0300

    rm stylex

--- vite.config.ts
+++ vite.config.ts
@@ -25,31 +25,6 @@ export default defineConfig(async ({ mode }) => {
                     ref: true,
                 },
             }),
-            stylex.default(),
-            {
-                name: 'custom',
-                async closeBundle() {
-                    const files = fs.readdirSync('dist/assets')
-                    const cssFile = files.find(f => f.startsWith('stylex.'))!
-                    fs.renameSync(`dist/assets/${cssFile}`, `dist/assets/${cssFile.replace('stylex.', '')}`)
-                    const html = fs.readFileSync(`dist/index.html`, 'utf-8')
-                    fs.writeFileSync(`dist/index.html`, html.replace('stylex.', ''))
-
-                    const newCssFilename = `dist/assets/${cssFile.replace('stylex.', '')}`
-                    let css = fs.readFileSync(newCssFilename, 'utf8')
-
-                    css = css.replace('@layer ', '@layer reset, ')
-
-                    const resetCss = fs.readFileSync('src/reset.css', 'utf8')
-
-                    css += '@layer reset {\n' + resetCss + '\n}\n'
-
-                    const newCss = await postcss([autoprefixer])
-                        .process(css, { from: newCssFilename, to: 'output.css' })
-                        .then(result => result.css)
-                    fs.writeFileSync(newCssFilename, newCss)
-                },
-            },
             viteStaticCopy({
                 targets: [
                     {
